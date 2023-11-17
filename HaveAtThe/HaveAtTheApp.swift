ed8ddd7d7e25a9aafa2e971eb69a06fb4a6fb23b
James Majors <noizetoys@yahoo.com>
2023-11-17 13:51:35 -0500

    Initial Commit

---
+++ HaveAtThe/HaveAtTheApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaveAtTheApp.swift
+//  HaveAtThe
+//
+//  Created by Apple User on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct HaveAtTheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
